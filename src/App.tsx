e6f7fc1c3041035a022acb15f5e510702e7d4628
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-11 23:48:34 +0100

    added about and store page to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/store" element={<Store/>}/>
       </Routes>
     </div>
   )
