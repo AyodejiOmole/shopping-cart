c4b72372dfafdfdfa525aa25784faed71a287709
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-11 23:50:22 +0100

    linked navbar to store and about page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const linkStyle = "text-sm font-bold hover:text-blue-600 font-mono leading-loose cursor-pointer";
 
@@ -9,9 +10,18 @@ export default function Navbar() {
     return (
         <div className="flex justify-between justify-center text-center align-center flex-col md:flex-row lg:flex-row border-2 border-gray-200 px-3 py-6 sticky lg:p-6">
             <div className="flex justify-between align-center lg:w-1/3 md:w-1/3 w-full">
-                <p className={linkStyle}>Home</p>
-                <p className={linkStyle}>Store</p>
-                <p className={linkStyle}>About</p>
+                <Link to="/">
+                    <p className={linkStyle}>Home</p>
+                </Link>
+                
+                <Link to="/store">
+                    <p className={linkStyle}>Store</p>
+                </Link>
+                
+                <Link to="/about">
+                    <p className={linkStyle}>About</p>
+                </Link>
+                
             </div>
 
             <p className="mt-4 md:mt-0 lg:mt-0">{cartCount}</p>
