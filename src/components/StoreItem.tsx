2bf54fb4dd659d54e416fbaad4e67b50ed27b52c
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-12 13:02:33 +0100

    added feature to get quantity

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { increaseCartQuantity, decreaseCartQuantity, removeFromCart } from "../store/slices/shoppingCartSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { CartItem } from "../store/slices/shoppingCartSlice";
 
 interface StoreItemProps {
     id: number
@@ -11,21 +12,36 @@ interface StoreItemProps {
 export default function StoreItem( { id, name, price, imgUrl}: StoreItemProps) {
     const dispatch = useDispatch();
 
+    const getItemQuantity = (id: number) => {
+        const value: undefined | number = useSelector((state: any) => state.cart).find((item: CartItem) => item.id === id)?.quantity;
+        if(value === undefined) {
+            return 0
+        } else { return value }
+    }
+
     return (
-        <div className="shadow-lg my-3 col-span-1 rounded-md cursor-pointer pb-3">
+        <div className="shadow-lg my-3 col-span-1 rounded-md cursor-pointer pb-3 h-[450px]">
             <img src={imgUrl} alt={name} className="h-[300px] w-full rounded-tr-md rounded-tl-md" />
 
-            <div className="flex flex-col text-center justify-center align-center">
+            <div className="flex flex-col text-center justify-center align-center px-4">
                 <h3 className="font-bold font-mono text-lg">{name}</h3>
 
-                <p className="text-sm font-medium">{`$${price}`}</p>
+                <p className="text-sm font-medium mt-3">{`$${price}`}</p>
 
-                <button 
-                    onClick={() => dispatch(increaseCartQuantity(id))}
-                    className=""
-                >
-                    Add to cart
-                </button>
+                <div>
+                    {
+                        getItemQuantity(id) !== 0
+                            ? (
+                                <p>added</p>
+                            ) 
+                            : (<button 
+                                onClick={() => dispatch(increaseCartQuantity(id))}
+                                className="w-full bg-blue-500 rounded p-3 mt-3"
+                            >
+                               + Add to cart
+                            </button>)
+                    } 
+                </div>
             </div>
         </div>
     )
