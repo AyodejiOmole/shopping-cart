be12717fdd9265ebcd6dd42f006ff729fdb27245
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-12 17:21:38 +0100

    added total feature to shopping cart drawer

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,19 +1,38 @@
-import { increaseCartQuantity, decreaseCartQuantity, removeFromCart } from "../store/slices/shoppingCartSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { CartItem } from "../store/slices/shoppingCartSlice";
-import { useState } from "react";
 import { useOpenCartContext } from "../context/OpenCartContext";
+import ShoppingCartItem from "./ShoppingCartItem";
+import storeItems from "../data/goods.json";
 
 export default function ShoppingCart() {
     const { isOpen, closeCart } = useOpenCartContext();
+    const cartItems = useSelector((state: any) => state.cart);
     
     return (
         <div className={`w-full top-0 h-auto rounded-sm bottom-0 flex right-0 top-0 z-10 ${isOpen === true ? "absolute" : "-translate-x-full hidden"}`}>
-            <div className="lg:w-2/3 h-full bg-transparent" onClick={() => closeCart()}></div>
-            <div className="lg:w-1/3 w-full h-full bg-white p-3 shadow-2xl border-2 border-gray-200 rounded-lg">
+            <div className="lg:w-2/3 md:w-1/2 h-full bg-transparent" onClick={() => closeCart()}></div>
+            <div className="lg:w-1/3 md:w-1/2 w-full h-full bg-white p-3 shadow-2xl border-2 border-gray-200 rounded-lg">
                 <div className="flex justify-between align-center">
                     <h3 className="text-xl font-bold">Cart</h3>
-                    <h3 className="text-xl font-semibold cursor-pointer" onClick={() => closeCart()}>x</h3>
+                    <h3 className="text-xl font-semibold cursor-pointer" onClick={() => closeCart()}>&times;</h3>
+                </div>
+
+                <div>
+                    {cartItems.count === 0 ? <p className="text-center text-black text-lg font-bold my-auto">You have no cart items!</p> : ""}
+                    {cartItems.map((item: CartItem) => (
+                        <ShoppingCartItem key={item.id} {...item}/>
+                    ))}
+                </div>
+
+                <div className="my-auto font-bold font-mono text-lg">
+                    Total{" "}
+                    {`$${
+                            cartItems.reduce((total: number, cartItem: CartItem) => {
+                                const item = storeItems.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0) * cartItem.quantity
+                            }, 0)
+                        }
+                    `}
                 </div>
             </div>
         </div>
--- src/components/ShoppingCartItem.tsx
+++ src/components/ShoppingCartItem.tsx
@@ -1,4 +1,39 @@
+import { CartItem } from "../store/slices/shoppingCartSlice";
+import storeItems from "../data/goods.json";
+import { removeFromCart } from "../store/slices/shoppingCartSlice";
+import { useDispatch } from "react-redux";
 
-export default function() {
+// interface CartItemProps {
+//     id: number,
+//     quantity: number
+// }
 
+export default function({ id, quantity}: CartItem) {
+    const dispatch = useDispatch();
+
+    const item = storeItems.find(item => item.id === id);
+    if(item == null) return null;
+
+    return (
+        <div className="flex justify-between align-center w-full my-3 h-[80px]">
+            <div className="flex align-center">
+                <img src={item?.imgUrl} alt={item?.name} className="w-1/2"/>
+                <div className="my-auto mx-2">
+                    <div className="flex">
+                        <p className="font-semibold text-md font-mono mx-2">{item?.name}</p>
+                        <p className="text-xs text-gray-400 my-auto">{`x${quantity}`}</p>
+                    </div>
+                    
+                    <p className="font-mono text-sm text-gray-400 mx-2 my-auto">{`$${item?.price}`}</p>
+                </div>
+            </div>
+            
+            <div className="flex align-center">
+                <p className="font-mono text-sm text-gray-400 mx-2 my-auto">{`$${item?.price * quantity}`}</p>
+                <div className="border-2 border-gray-200 hover:border-blue-500 group text-center rounded h-1/2 text-center my-auto px-3 cursor-pointer" onClick={() => dispatch(removeFromCart(id))}>
+                    <p className="flex align-center justify-center group-hover:text-blue-500 font-semibold">&times;</p>
+                </div>
+            </div>
+        </div>
+    )
 }
